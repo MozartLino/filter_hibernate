1f1dece81406292d63185ecf922b18ce2739f05e
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-07-26 00:47:37 -0300

    create hibernateUtil

--- src/test/java/br/com/lino/dao/PaiDAOTest.java
+++ src/test/java/br/com/lino/dao/PaiDAOTest.java
@@ -5,19 +5,17 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import br.com.lino.model.Filho;
 import br.com.lino.model.Pai;
+import br.com.lino.util.HibernateUtil;
 
 public class PaiDAOTest {
 
 	public Session session;
-	private SessionFactory sessionFactory;
 
 	@Test
 	public void deveTrazerPaiContendoOsFilhosComIdadeMaiorQue10() {
@@ -25,9 +23,7 @@ public class PaiDAOTest {
 				Arrays.asList(
 						new Filho("Filho 1", 10),
 						new Filho("Filho 2", 14),
-						new Filho("Filho 3", 9)
-						)
-				);
+						new Filho("Filho 3", 9)));
 
 		session.save(pai);
 		session.flush();
@@ -39,18 +35,15 @@ public class PaiDAOTest {
 
 	@Before
 	public void setUp() {
-		sessionFactory = new Configuration().configure().buildSessionFactory();
-		session = sessionFactory.openSession();
-		session.createSQLQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
-		session.createSQLQuery("truncate table Pai").executeUpdate();
-		session.createSQLQuery("truncate table Filho").executeUpdate();
-		session.createSQLQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
+		session = HibernateUtil.getCurrentSession();
+		HibernateUtil.beginTransaction();
 	}
 
 	@After
 	public void setDown() {
-		session.close();
-		sessionFactory.close();
+		HibernateUtil.rollbackTransaction();
+		HibernateUtil.closeSession();
 	}
+
 
 }
